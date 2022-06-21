3b75ae8df0da26b7051ed6649054c55a8feca0db
RabieRAZINE <rrazine94@gmail.com>
2022-06-21 15:51:02 +0200

    firebase settings

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'bookshelves';
+  constructor() {
+    var config =  {
+      apiKey: "AIzaSyCEtpg9qpCoz5p9wTiUz2JnVtTu2H9NLts",
+      authDomain: "bookshelves-angular-firebase.firebaseapp.com",
+      projectId: "bookshelves-angular-firebase",
+      storageBucket: "bookshelves-angular-firebase.appspot.com",
+      messagingSenderId: "830231894073",
+      appId: "1:830231894073:web:9017cfcfa616297074d5e8",
+      measurementId: "G-C0RF7LQRKL"
+    };
+    firebase.initializeApp(config);
+  }
 }
