0fe4d7d88d10c8f8d3c8ee40881aad1a6d794ab3
RabieRAZINE <rrazine94@gmail.com>
2022-06-24 10:36:15 +0200

    bug

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -8,18 +8,5 @@ export class AuthService {
 
   constructor() { }
 
-  createNewUser(email: string, password: string ) {
-    return new Promise(
-      (resolve, reject) => {
-        firebase.auth().createUserWithEmailAndPassword(email, password).then(
-          () => {
-            resolve();
-          },
-          (error) => {
-            reject(error);
-          }
-        )
-      }
-    )
-  }
+  
 }
